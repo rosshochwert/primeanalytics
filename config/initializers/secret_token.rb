355876e5bdaa9033f986f6f23919ed467bc65123
Ross Hochwert <rosshochwert@gmail.com>
2013-06-02 21:32:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primeanalytics::Application.config.secret_token = '54e0897067bad0b7d8af1a3b43a3850eef0f2b42f24454e5ae9d8c0de18142d44621f4c2d66e50d35f65f18498c29c0053930e9d8d0ba28dd612522f540db9d9'
